2ed6a2483a83f02492b4871a65158f8d3793a582
Aleksandra Okrój <61236678+aleksandraokroj@users.noreply.github.com>
2020-04-29 14:42:03 +0200

    Add files via upload

---
+++ Silnia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Silnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe");
+            int liczba = int.Parse(Console.ReadLine());
+
+            int silnia = 1;
+            for (int i= 1; i <= liczba; i++)
+            {
+                silnia *= i;
+            }
+            Console.WriteLine("Silnia dla liczby"+ liczba +"to"+ silnia);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
