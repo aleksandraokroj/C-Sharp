7b2f2a2c9ad377afad7b8d678d013733e56ecb5c
Aleksandra Okrój <61236678+aleksandraokroj@users.noreply.github.com>
2020-04-29 14:43:30 +0200

    Add files via upload

---
+++ TotoLotek/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace TotoLotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] tablica = new int[6];
+            Random generator = new Random();
+
+            int i = 0;
+            while(i<6)
+            {
+                int liczba = generator.Next(1, 49);
+                if(!tablica.Contains(liczba))
+                {
+                    tablica[i] = liczba;
+                    i++;
+                }
+            }
+            Console.WriteLine("Wylosowane liczby to:");
+            foreach (int liczbaa in tablica)
+            {
+                Console.Write("{0} ,", liczbaa);
+            }
+            Console.ReadKey();
+           
+
+        }
+    }
+}
