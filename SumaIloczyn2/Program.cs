76d28296ffa047f03aa6033a3acba2d92f8bb6ee
Aleksandra Okrój <61236678+aleksandraokroj@users.noreply.github.com>
2020-04-29 14:49:57 +0200

    Add files via upload

---
+++ SumaIloczyn2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumaIloczyn2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n, k, a, iloczyn, suma;
+            iloczyn = 1;
+            suma = 0;
+            Console.WriteLine("Podaj wartość zmiennej n:");
+            n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Podaj wartość zmiennej k:");
+            k = Convert.ToUInt32(Console.ReadLine());
+            a = k;
+            for (int i = 0; i < n; i++)
+            {
+                suma += a;
+                checked
+                {
+                    iloczyn *= a;
+                }
+                a += k;
+
+            }
+            Console.WriteLine("suma=" + suma);
+            Console.WriteLine("iloczyn=" + iloczyn);
+            Console.ReadKey();
+
+        }
+    }
+}
