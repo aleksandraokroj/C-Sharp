1fdd868b0b129017b11c2a3a7cabf8015a657774
Aleksandra Okrój <61236678+aleksandraokroj@users.noreply.github.com>
2020-04-29 14:44:33 +0200

    Add files via upload

---
+++ Wyrazenie2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Wyrazenie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Podaj wartość x:");
+                x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartość y:");
+                y = int.Parse(Console.ReadLine());
+
+            if (x < 0 && y < 0)
+            { z = x * y; }
+            else if (x == 0 || y == 0)
+            { z = 10; }
+            else z = x + y;
+           
+            Console.WriteLine("Z wynosi: {0}", z);
+            Console.ReadKey();
+
+                
+
+
+        }
+    }
+}
