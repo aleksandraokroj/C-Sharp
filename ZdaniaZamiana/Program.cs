30622c31bf871563527c1da36bd86ff68c611a0d
Aleksandra Okrój <61236678+aleksandraokroj@users.noreply.github.com>
2020-04-29 14:46:59 +0200

    Add files via upload

---
+++ ZdaniaZamiana/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zdaniazamiana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napisz zdanie");
+            string[] text = Console.ReadLine().Split(" ");
+            string[] text2 = new string[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                text2[i] = text[i];
+            }
+            string słowo;
+            słowo = text[text.Length - 1];
+            text2[text.Length - 1] = text[0];
+            text2[0] = słowo;
+
+            foreach(string litera in text2){
+
+                Console.Write(litera);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
